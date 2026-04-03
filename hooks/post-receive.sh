81100e21475533a6892dd65cc9b1cf52b7d47936
VisionElf <waserk3@gmail.com>
2026-04-03 17:37:54 +0200

    fix

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -18,6 +18,8 @@ REPO_DIR="$HOME_DIR/git/wk-server-web.git"
 APP_DIR="$HOME_DIR/apps/wk-server-web"
 FRONT_DIR="$APP_DIR/frontend/wk-frontend"
 
+WEB_ROOT="/var/www/wk-server-web"
+
 echo "--> Checkout code"
 git --work-tree="$APP_DIR" --git-dir "$REPO_DIR" checkout -f "$BRANCH"
 
@@ -30,6 +32,11 @@ echo "--> Build frontend"
 cd "$FRONT_DIR"
 npm ci
 npm run build
+
+echo "--> Move Front to /var/www/"
+
+mkdir -p "$WEB_ROOT"
+rsync -a --delete "$FRONT_DIR/dist/" "$WEB_ROOT/"
 
 echo "--> Build and restart backend container"
 cd "$APP_DIR"
