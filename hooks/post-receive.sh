f4eba66c7e812be57fa35d9a14cd853ef5bbd3c6
VisionElf <waserk3@gmail.com>
2026-04-03 17:12:24 +0200

    fix

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
-export NVM_DIR="/home/waserk/.nvm"
+echo "=== POST RECEIVE START ==="
+
 source "$NVM_DIR/nvm.sh"
 nvm use 20
+
+echo "USER=$(whoami)"
+echo "HOME=$HOME"
+which node
+node -v
 
 BRANCH="main"
 REPO_DIR="/home/waserk/git/wk-server-web.git"
@@ -11,24 +17,25 @@ REPO_DIR="/home/waserk/git/wk-server-web.git"
 APP_DIR="/home/waserk/apps/wk-server-web"
 FRONT_DIR="$APP_DIR/frontend/wk-frontend"
 
+echo "--> Checkout code"
 git --work-tree="$APP_DIR" --git-dir "$REPO_DIR" checkout -f "$BRANCH"
 
-echo "=== Deploy start ==="
+echo "--> Deploy start ==="
 
 cd "$APP_DIR"
 
-echo "[1/3] Build frontend"
+echo "--> Build frontend"
 
 cd "$FRONT_DIR"
 npm ci
 npm run build
 
-echo "[2/3] Build and restart backend container"
+echo "--> Build and restart backend container"
 cd "$APP_DIR"
 docker compose down
 docker compose up -d --build
 
-echo "[3/3] Reload nginx"
+echo "--> Reload nginx"
 sudo nginx -t
 sudo systemctl reload nginx
 
